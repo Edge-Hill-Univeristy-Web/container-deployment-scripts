4aa2318a072bb0ebca5f5cc92a4ae9459dcbe384
Danny2097 <danielcampbell2097@hotmail.com>
2022-10-10 10:29:16 +0100

    initial upload of deploy scripts for windows and unix systems

---
+++ deploy.bat
@@ -0,0 +1,52 @@
+:: This script will deploy the EHU CIS docker containers.
+:: Author : Dan Campbell
+::
+ 
+@echo off
+if %1. ==. goto error
+ 
+if not %1. ==. goto deployment
+ 
+:error
+SET msgboxTitle=Error
+SET msgboxBody='Missing numerical module code argument! Unable to proceed, please include a numerical module code as an argument.'
+SET tmpmsgbox=%temp%\~tmpmsgbox.vbs
+IF EXIST "%tmpmsgbox%" DEL /F /Q "%tmpmsgbox%"
+ECHO msgbox "%msgboxBody%",0,"%msgboxTitle%">"%tmpmsgbox%"
+WSCRIPT "%tmpmsgbox%"
+goto :eof
+ 
+:end
+ 
+ 
+:deployment
+@echo off
+set /A year=22
+set /A module= %1
+ 
+if exist cis-docker\docker-compose.yml (
+  echo [INFO] A directory and docker-compose.yml already exists for cis%module%
+) else (
+  echo [INFO] Downloading...
+  git clone https://github.com/Edge-Hill-Univeristy-Web/cis-docker.git
+)
+ 
+echo [INFO] Changing Directory
+cd cis-docker
+ 
+echo [INFO] Creating environment variable file
+ 
+@echo off
+echo COMPOSE_PROJECT_NAME=EHU_Containers > ".env"
+echo MODULE_CODE=%module% >> ".env"
+echo YEAR=%year% >> ".env"
+ 
+echo [INFO] created .env
+echo [INFO] Deploying container for CIS%module%
+ 
+docker-compose up
+ 
+:end
+ 
+:eof
+:end
---
+++ deploy.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+##################################################################
+# This script will deploy the EHU CIS docker containers 
+
+# Flags
+#       -h = Help
+#       -m = Module Code
+#       -y = Year
+
+# Author : Dan Campbell
+##################################################################
+while getopts hm: flag
+do
+    case "${flag}" in
+        h)  echo "deploy-cis-containers."
+            echo "Usage    :    ./deploy-cis-container.sh -m <modulecode> -y <year>"
+            echo "Example  :    ./deploy-cis-container.sh -m 2152 -y 22"
+            echo
+            echo "Syntax   :  [-h|m]"
+            echo "options  :"
+            echo
+            echo "-h     prints help"
+            echo
+            echo "The following flags are required for the script to function"
+            echo "-m     Module code [example 2152]"
+            exit;;
+        m) modulecode=${OPTARG};;
+        y) year=22;;
+        \?) # incorrect option
+         echo "Error: Invalid option"
+         exit;
+    esac
+done
+
+# Checks for values!
+if [ -z "$year" ] ; then
+    echo "[ERROR] please specify a value for YEAR using the -y flag"
+fi
+
+if [ -z "$modulecode" ] ; then
+    echo "[ERROR] please specify a value for MODULECODE using the -m flag"
+fi
+
+if [ -z "$modulecode" ] || [ -z "$year" ] ; then
+    exit;
+fi
+
+echo "[INFO] Beginning Deployment of "$modulecode""
+
+FILE="EHU-CIS-Docker/docker-compose.yml"
+
+# Check if docker-compose.yml exists
+if [ -f "$FILE" ]; then
+    echo "[INFO] $FILE exists. Proceeding to deploy"
+else 
+    echo "[INFO] $FILE does not exist. Proceeding to download..."
+    git clone https://github.com/Edge-Hill-Univeristy-Web/EHU-CIS-Docker.git
+fi
+
+cd EHU-CIS-Docker
+
+if [ -f ".env" ] ; then
+    echo "[INFO] .env file already exists. Proceeding to remove"
+    rm .env
+fi
+
+# Creates .env based on flags
+cat <<EOT >> .env
+COMPOSE_PROJECT_NAME=EHU_Containers
+MODULE_CODE="$modulecode"
+YEAR="$year"
+EOT
+
+echo "[INFO] Created environment variables"
+echo "[INFO] Deploying CIS$modulecode Container"
+echo
+
+docker-compose up -d
